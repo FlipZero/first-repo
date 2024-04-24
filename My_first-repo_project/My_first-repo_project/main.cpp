35fed8342063e2f917b1edcd26da9de7f5df5745
Filip <filippo.kazmierczak@gmail.com>
2024-04-24 10:11:38 +0200

    Ręczne scalenie z innym branchem

--- My_first-repo_project/My_first-repo_project/main.cpp
+++ My_first-repo_project/My_first-repo_project/main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include "Window.h"
+
+int main()
+{
+    std::cout << "Creating Window\n";
+
+    Window* pWindow = new Window();
+
+    bool running = true;
+    while (running)
+    {
+        if (!pWindow->ProcessMessages())
+        {
+            std::cout << "Closing Window\n";
+            running = false;
+        }
+
+        Sleep(10);
+    }
+
+    delete pWindow;
+
+    return 0;
+}
+
+
+/*
+#include <iostream>
 
 int main()
 {
@@ -39,3 +67,4 @@ int main()
     }
     return 0;
 }
+*/
